35b4420e8136ac89817633a588ab62e904e76d30
Khanh <ngokhanh809@gmail.com>
2026-07-12 10:59:10 -0700

    Finished neural network implementation

--- neuralnet.h
+++ neuralnet.h
@@ -83,9 +83,29 @@ void NeuralNet_FeedForward(neuralnet *NN, neuralnet_activation_fn Fn);
 #include <stdlib.h> /* malloc, free */
 #include <stdint.h>
 #include <assert.h>
+#include <time.h>
 
 
+#define NN__ALLOC(p_nn, size_bytes) \
+    (p_nn)->AllocatorCallback(\
+        (p_nn)->AllocatorData, \
+        &(neuralnet_allocator_param) { \
+            .Mode = NNALLOC_ALLOCATE, \
+            .Allocate.SizeBytes = (size_bytes)\
+        }\
+    )
+#define NN__FREE(p_nn, ptr) \
+    (p_nn)->AllocatorCallback(\
+        (p_nn)->AllocatorData,\
+        &(neuralnet_allocator_param) {\
+            .Mode = NNALLOC_FREE, \
+            .Free.Ptr = (ptr), \
+        }\
+    )
 static void *NN__DefaultAllocatorCallback(void *Data, neuralnet_allocator_param *Param);
+static void NN__Randomize(neuralnet *NN);
+static float NN__GetRandomValue(void);
+static float NN__StepFn(float Bias, float Value);
 
 
 neuralnet NeuralNet_Create(const neuralnet_config *Config)
@@ -108,6 +128,8 @@ neuralnet NeuralNet_Create(const neuralnet_config *Config)
 
     /* allocate needed mem */
     {
+        NN.Inputs = NN__ALLOC(&NN, sizeof(NN.Inputs[0]) * NN.InputCount);
+        NN.Layers = NN__ALLOC(&NN, sizeof(NN.Layers[0]) * NN.LayerCount);
         int PrevLayerNodeCount = Config->InputCount;
         for (int i = 0; i < Config->LayerCount; i++)
         {
@@ -115,26 +137,69 @@ neuralnet NeuralNet_Create(const neuralnet_config *Config)
             int WeightCount = PrevLayerNodeCount*CurrLayerNodeCount;
             int BiasCount = CurrLayerNodeCount;
 
-            /* TODO: allocate these in order */
-            sizeof(NN.Layers[0]);
-            WeightCount*sizeof(NN.Layers[0].Weights[0]);
-            BiasCount*sizeof(NN.Layers[0].NodeBiases[0]);
-            CurrLayerNodeCount*sizeof(NN.Layers[0].NodeValues[0]);
+            NN.Layers[i].Weights = NN__ALLOC(&NN, WeightCount*sizeof(NN.Layers[0].Weights[0]));
+            NN.Layers[i].NodeBiases = NN__ALLOC(&NN, BiasCount*sizeof(NN.Layers[0].NodeBiases[0]));
+            NN.Layers[i].NodeValues = NN__ALLOC(&NN, CurrLayerNodeCount*sizeof(NN.Layers[0].NodeValues[0]));
 
             PrevLayerNodeCount = CurrLayerNodeCount;
         }
     }
+
+    NN__Randomize(&NN);
     return NN;
 }
 
-void NeuralNet_FeedForward(neuralnet *NN, neuralnet_activation_fn Fn)
+void NeuralNet_Destroy(neuralnet *NN)
+{
+    for (int i = 0; i < NN->LayerCount; i++)
+    {
+        NN__FREE(NN, NN->Layers[i].Weights);
+        NN__FREE(NN, NN->Layers[i].NodeBiases);
+        NN__FREE(NN, NN->Layers[i].NodeValues);
+    }
+    NN__FREE(NN, NN->Layers);
+    NN__FREE(NN, NN->Inputs);
+}
+
+void NeuralNet_FeedForward(neuralnet *NN, neuralnet_activation_fn ActivationFn)
 {
 #if 0
-    for each level:
-        for each node:
-            value = accumulate prev node * weights to this node
-            node true value = activation_fn(bias, value);
+    /* NOTE: pseudocode: */
+    for each layer:
+        for each node - k:
+            accum = 0;
+            for each prev node - i:
+                accum += node[i] * weights[i][k]
+            node[k] = activation_fn(bias[k], accum)
 #endif
+
+
+    if (ActivationFn == NULL)
+    {
+        ActivationFn = NN__StepFn;
+    }
+
+    float *PrevNodes = NN->Inputs;
+    int PrevNodeCount = NN->InputCount;
+    for (int n = 0; n < NN->LayerCount; n++)
+    {
+        for (int k = 0; k < NN->Layers[n].NodeCount; k++)
+        {
+            float Accum = 0;
+            for (int i = 0; i < PrevNodeCount; i++)
+            {
+                Accum += PrevNodes[i] * NN->Layers[n].Weights[k*PrevNodeCount + i];
+            }
+
+            NN->Layers[n].NodeValues[k] = ActivationFn(
+                NN->Layers[n].NodeBiases[k], 
+                Accum
+            );
+        }
+
+        PrevNodes = NN->Layers[n].NodeValues;
+        PrevNodeCount = NN->Layers[n].NodeCount;
+    }
 }
 
 
@@ -146,6 +211,38 @@ static void *NN__DefaultAllocatorCallback(void *Data, neuralnet_allocator_param 
     case NNALLOC_FREE:     free(Param->Free.Ptr); break;
     }
     return NULL;
+}
+
+static void NN__Randomize(neuralnet *NN)
+{
+    srand(time(NULL));
+    int PrevNodeCount = NN->InputCount;
+    for (int n = 0; n < NN->LayerCount; n++)
+    {
+        for (int k = 0; k < NN->Layers[n].NodeCount; k++)
+        {
+            NN->Layers[n].NodeBiases[k] = NN__GetRandomValue();
+            NN->Layers[n].NodeValues[k] = NN__GetRandomValue();
+            for (int i = 0; i < PrevNodeCount; i++)
+            {
+                NN->Layers[n].Weights[k*PrevNodeCount + i] = NN__GetRandomValue();
+            }
+        }
+
+        PrevNodeCount = NN->Layers[n].NodeCount;
+    }
+}
+
+static float NN__GetRandomValue(void)
+{
+    return (float)rand() / RAND_MAX;
+}
+
+static float NN__StepFn(float Bias, float Value)
+{
+    if (Value > Bias)
+        return 1.0;
+    return 0;
 }
 
 #endif
